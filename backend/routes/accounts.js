9a54abd1d9f75c1a7479071622c63e6b1800f887
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2024-07-30 02:10:57 +0530

    Final BACKEND commit

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -4,43 +4,48 @@ const { authMiddleware } = require("../auth");
 const accountRoute = Router();
 
 accountRoute.get("/balance", authMiddleware, async (req, res) => {
-
-    const { balance } = await Account.findOne({
+    const account = await Account.findOne({
         userId: req.userId,
-    })
+    });
+
+    if (!account) {
+        return res.status(404).json({
+            msg: "Account not found"
+        });
+    }
+
     res.status(200).json({
-        balance: balance
-    })
-})
+        balance: account.balance
+    });
+});
 
 accountRoute.post("/transfer", authMiddleware, async (req, res) => {
-
-    const {to, amount} = req.body;
+    const { to, amount } = req.body;
 
     const fromUser = await Account.findOne({
         userId: req.userId
-    })
+    });
 
     const toUser = await Account.findOne({
         userId: to
-    })
+    });
 
     if (!fromUser) {
         return res.status(400).json({
             msg: "User does not exist"
-        })
+        });
     }
 
     if (!toUser) {
         return res.status(400).json({
             msg: "User does not exist"
-        })
+        });
     }
 
-    if (!(amount >= fromUser.balance)) {
+    if (amount > fromUser.balance) {
         return res.status(400).json({
             msg: "Insufficient Balance"
-        })
+        });
     }
 
     await Account.updateOne({
@@ -49,22 +54,21 @@ accountRoute.post("/transfer", authMiddleware, async (req, res) => {
         $inc: {
             balance: -amount
         }
-    })
+    });
 
     await Account.updateOne({
-        userId: toUser
+        userId: to
     }, {
         $inc: {
             balance: amount
         }
-    })
-    
+    });
+
     res.status(200).json({
         msg: "Transaction Successful"
-    })
-
-})
+    });
+});
 
 module.exports = {
     accountRoute,
-}
+};
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,98 +3,122 @@ const { userValidation, userExists, userValidationSignIn, userExistsSignIn, auth
 const { User, Account } = require("../db");
 const userRouter = Router();
 const jwt = require("jsonwebtoken");
-const { JWT_SECRET } = require("../config")
-const zod = require("zod")
+const { JWT_SECRET } = require("../config");
+const zod = require("zod");
 
 userRouter.post("/signup", userValidation, userExists, async (req, res) => {
-    const {username, password, firstName, lastName} = req.body;
+    try {
+        const { username, password, firstName, lastName } = req.body;
 
         const user = await User.create({
-            username: username,
-            password: password,
-            firstName: firstName,
-            lastName: lastName
-        })
+            username,
+            password,
+            firstName,
+            lastName
+        });
+
+        const userId = user._id;
 
         await Account.create({
             userId, 
-            balance: 1 + Math.random() * 10000
-        })
+            amount: 1 + Math.random() * 10000 
+        });
 
-        const userID = user._id;
-
-        const token = jwt.sign({userID}, JWT_SECRET)
+        const token = jwt.sign({ userId }, JWT_SECRET);
 
         res.status(200).json({
-        message: "User created successfully",
-        token: token
-    })
-})
+            message: "User created successfully",
+            token
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating user",
+            error: error.message
+        });
+    }
+});
 
-userRouter.post("/signin", userValidationSignIn, userExistsSignIn, authMiddleware, async (req, res) => {
-    const {username, password} = req.body;
+userRouter.post("/signin", userValidationSignIn, userExistsSignIn, async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-    const user = await User.findOne({username, password});
+        const user = await User.findOne({ username, password });
 
-    if (user) {
-        const token = jwt.sign({userID: user._id}, JWT_SECRET);
-        res.status(200).json({
-        token: token
-    })
-    return;
-}
-    res.status(411).json({
-        message: "Error while logging in"
-    })
-
-})
+        if (user) {
+            const token = jwt.sign({ userId: user._id }, JWT_SECRET);
+            res.status(200).json({
+                token
+            });
+            return;
+        }
+        res.status(401).json({
+            message: "Error while logging in"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error signing in",
+            error: error.message
+        });
+    }
+});
 
 const updateBody = zod.object({
     password: zod.string().min(6),
     firstName: zod.string(),
     lastName: zod.string(),
-})
+});
 
 userRouter.put("/", authMiddleware, async (req, res) => {
-    const success = updateBody.safeParse(req.body);
+    const parsed = updateBody.safeParse(req.body);
 
-    if (!success) {
-        res.status(411).json({
-            msg: "Error while updating information"
-        })
+    if (!parsed.success) {
+        res.status(400).json({
+            msg: "Error while updating information",
+            error: parsed.error.errors
+        });
+        return;
     }
-    await User.updateOne({_id: req.userID}, req.body);
-    res.status(200).json({
-        msg: "User updated successfully"
-    })
-})
+
+    try {
+        await User.updateOne({ _id: req.userID }, req.body);
+        res.status(200).json({
+            msg: "User updated successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error updating user",
+            error: error.message
+        });
+    }
+});
 
 userRouter.get("/bulk", async (req, res) => {
-    const filter = req.query.filter || ""
+    try {
+        const filter = req.query.filter || "";
 
-    const users = await User.find({
-        $or: [
-    {
-        firstName: {
-            "$regex": filter 
-        }, lastName: {
-            "$regex": filter 
-        }
+        const users = await User.find({
+            $or: [
+                { firstName: { "$regex": filter} },
+                { lastName: { "$regex": filter} }
+            ]
+        });
+
+        res.status(200).json({
+            user: users.map(user => ({
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                _id: user._id
+            }))
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching users",
+            error: error.message
+        });
     }
-  ]
-    })
-
-    res.status(200).json({
-        user: users.map(user => ({
-            username: user.username,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            _id: user._id
-        }))
-    })
-})
+});
 
 module.exports = {
     userRouter,
-}
-
+};
