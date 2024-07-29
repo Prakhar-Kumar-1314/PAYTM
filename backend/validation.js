8afe679e476097b99d87b4a731fecfc9a0daf229
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2024-07-29 19:24:52 +0530

    signin

--- backend/validation.js
+++ backend/validation.js
@@ -43,8 +43,48 @@ const userExists = async (req, res, next) => {
     }
 };
 
+const userExistsSignIn = async (req, res, next) => {
+    const { username, password } = req.body;
+    
+    try {
+        const exists = await User.findOne({ username, password });
+        if (!exists) {
+            return res.status(400).json({ 
+                msg: "User Does not Exist"
+            });
+        }
+        next();
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        });
+    }
+};
+
+const userValidationSignIn = (req, res, next) => {
+    const {username, password} = req.body;
+
+    try {
+        zod.object({
+            username: zod.string().email(),
+            password: zod.string().min(6)
+        }).parse({
+            username: username,
+            password: password,
+        })
+        next();
+    } catch (err) {
+        res.status(411).json({
+            msg: "Incorrect Inputs",
+            error: err.message
+        })
+    }
+}
+
 
 module.exports = {
     userExists,
-    userValidation
+    userValidation,
+    userExistsSignIn,
+    userValidationSignIn,
 }
